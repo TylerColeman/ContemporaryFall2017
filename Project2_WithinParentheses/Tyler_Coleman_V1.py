d6ec07233403a13d09636e90cd7be6c131dd9990
root <tylerblake.tc@gmail.com>
2017-09-22 10:46:18 -0500

    Progress

--- Project2_WithinParentheses/Tyler_Coleman_V1.py
+++ Project2_WithinParentheses/Tyler_Coleman_V1.py
@@ -9,6 +9,37 @@ all text within parentheses,\nThe number of lines in the document\
 All lines will be sorted according to the first characters in the first word."
     file.write(head)
 
+def find_parentheses(chars):
+    string_list = []
+    for index in range(int(len(chars))):
+        i = 1
+        string = ""
+        if chars[index] == '(':
+            string = string + chars[index]
+            index += 1
+            while chars[index] != ')':
+                if chars[index] == ' ':
+                    i += 1
+                string = string + chars[index]
+                index += 1
+            string_list.append(string + ')' + str(i))
+    return string_list
+
+def main():
+    filename = "input.txt"
+    f = open(filename, 'r')
+    chars = []
+    for line in f:
+        for char in line:
+            chars.append(char)
+    parenth_strings_list = find_parentheses(chars)
+    num_lines = int(len(parenth_strings_list))
+    print(parenth_strings_list)
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
