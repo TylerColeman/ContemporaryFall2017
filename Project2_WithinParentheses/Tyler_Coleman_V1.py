6ddf83a504ee296a8dcde4a596c5ee210229253f
Tyler Coleman <tylerblake.tc@gmail.com>
2017-09-25 21:17:30 -0500

    Good progress

--- Project2_WithinParentheses/Tyler_Coleman_V1.py
+++ Project2_WithinParentheses/Tyler_Coleman_V1.py
@@ -6,8 +6,8 @@ def header(file):
     head = "Name: Tyler Coleman\nExplanation: This program will scan a file and find:\n\
 all text within parentheses,\nThe number of lines in the document\
  that were contained in parentheses, and\nThe number of words in each line.\n\
-All lines will be sorted according to the first characters in the first word."
-    file.write(head)
+All lines will be sorted according to the first characters in the first word.\n\n"
+    file.write("%s" % head)
 
 def find_parentheses(chars):
     string_list = []
@@ -15,26 +15,36 @@ def find_parentheses(chars):
         i = 1
         string = ""
         if chars[index] == '(':
-            string = string + chars[index]
+            #string = string + chars[index]
             index += 1
             while chars[index] != ')':
                 if chars[index] == ' ':
                     i += 1
-                string = string + chars[index]
+                if chars[index] != '\n':
+                    string = string + chars[index]
                 index += 1
-            string_list.append(string + ')' + str(i))
+            string_list.append(string + ')  ' + str(i))
     return string_list
 
+def sort_list(parenth_lines):
+    parenth_lines.sort(key = str.lower)
+    new_list = ["({0}\n".format(i) for i in parenth_lines]
+    num_lines = int(len(parenth_lines))
+    new_list.append('\n' + str(num_lines))
+    return new_list
+
 def main():
-    filename = "input.txt"
+    filename = "alice.txt"
     f = open(filename, 'r')
+    f2 = open("output.txt", 'w')
     chars = []
     for line in f:
         for char in line:
             chars.append(char)
     parenth_strings_list = find_parentheses(chars)
-    num_lines = int(len(parenth_strings_list))
-    print(parenth_strings_list)
+    final_list = sort_list(parenth_strings_list)
+    header(f2)
+    f2.write(''.join(final_list))
 
 if __name__ == '__main__':
     main()
