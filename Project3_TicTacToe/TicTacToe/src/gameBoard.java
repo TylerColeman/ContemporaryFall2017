c28bfa36b8fc73702ae8c78401c3956ea66364fe
root <tylerblake.tc@gmail.com>
2017-10-24 11:49:42 -0500

    Project 3 progress

--- Project3_TicTacToe/TicTacToe/src/gameBoard.java
+++ Project3_TicTacToe/TicTacToe/src/gameBoard.java
@@ -1,4 +1,87 @@
 
 public class gameBoard {
+	//private vars
+	private char[][] board;
+	private int boardSize = 5;
+	private int trueSize = 3;
+	
+	//default constructor
+	gameBoard(){
+		//make a new 2D array 5x5
+		board = new char[boardSize][boardSize];
+		
+		for(int i = 0; i < boardSize; i++) {
+			for(int j = 0; j < boardSize; j++) {
+				if(i % 2 == 0)
+				{
+					if(j % 2 == 0) {
+						board[i][j] = '*';
+					}
+					else {
+						board[i][j] = '|';
+					}
+				}
+				else {
+					board[i][j] = '-';
+				}
 
+			}
+		}
+	}
+	/*
+	 * @Method:
+	 * @Parameters:
+	 * @Returns:
+	 * @Description:
+	 * 
+	 */
+	boolean makeMove(int i, int j, char value) {
+		if(i > trueSize || j > trueSize || i < 1 || j < 1) {
+			return false;
+			}
+		else {
+			if(i == 1) {i = 0;}
+			else if(i == 3) {i = 4;}
+			if(j == 1) {j = 0;}
+			else if(j == 3) {j = 4;}
+			if(board[i][j] == '*') {
+				board[i][j] = value;
+				return true;
+			}
+			else {
+				return false;
+			}
+			
+			
+		}
+		
+	}
+	/*
+	 * @Method:
+	 * @Parameters:
+	 * @Returns:
+	 * @Description:
+	 * 
+	 */
+	boolean check() {
+		int xCount;
+		int yCount;
+		return true;
+	}
+	/*
+	 * @Method:
+	 * @Parameters:
+	 * @Returns:
+	 * @Description:
+	 * 
+	 */
+	void printBoard() {
+		for(int i = 0; i < boardSize; i++) {
+			for(int j = 0; j < boardSize;j++) {
+				System.out.print(board[i][j]);
+				System.out.print(' ');
+			}
+			System.out.print('\n');
+		}
+	}
 }
--- Project3_TicTacToe/TicTacToe/src/main.java
+++ Project3_TicTacToe/TicTacToe/src/main.java
@@ -1,4 +1,72 @@
+import java.util.Scanner;
 
 public class main {
+	public static void main(String[] args) {
+		//scanner for input
+		Scanner playerInput = new Scanner(System.in);
+		//make a new gameboard
+		gameBoard ticTacToe = new gameBoard();
+		//bools
+		boolean game, victory, player1Turn, validMove;
+		//player input coordinates
+		int input1, input2;
+		
+		game = true;
+		while(game) {
+			//header message
+			System.out.println("Tyler Coleman");
+			System.out.println("Tyler Coleman");
+			System.out.println("Tyler Coleman");
+			System.out.println("Tyler Coleman");
+			
+			System.out.println("Welcome to TicTacToe!");
+			System.out.println("Player 1 is X's and player 2 is O's.");
+			System.out.println("To make a move just enter 2 integers between 1 and 3 (inclusive) seperated by a space.");
+			//start a match
+			victory = false;
+			player1Turn = true;
+			while(!victory) {
+				//player1's turn
+				ticTacToe.printBoard();
+				if(player1Turn) {
+					System.out.println("It is player 1's turn! Please make your move.");
+					validMove = false;
+					while(!validMove){
+						input1 = playerInput.nextInt();
+						input2 = playerInput.nextInt();
+						if(ticTacToe.makeMove(input1, input2, 'X')) {
+							System.out.println("Valid Move!");
+							validMove = true;
+						}
+						else {
+							System.out.println("Invalid Move! Try again!");
+							ticTacToe.printBoard();
+						}
+					}
+					player1Turn = false;
+				}
+				else {
+					System.out.println("It is player 2's turn! Please make your move.");
+					validMove = false;
+					while(!validMove){
+						
+						input1 = playerInput.nextInt();
+						input2 = playerInput.nextInt();
+						if(ticTacToe.makeMove(input1, input2, 'O')) {
+							System.out.println("Valid Move!");
+							validMove = true;
+						}
+						else {
+							System.out.println("Invalid Move! Try again!");
+							ticTacToe.printBoard();
+						}
+					}
+					player1Turn = true;
+				}
+			}
+		}
+	}
+
 
 }
+
